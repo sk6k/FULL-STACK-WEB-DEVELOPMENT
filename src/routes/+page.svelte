d8bd3721c2fd17a053b7871ff6d197d635677d82
Andrii Lisostep <reyandrey@mail.com>
2023-08-12 22:35:08 +0300

    Develom HTML structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Hello and welcome to my site!</h1>
+<a href="/about">About my site</a> -->
+<script>
+    import TodoItem from "../lib/todo-item.svelte"
+    
+    const title = "Todo | New"
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<form action="" method="">
+    <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo">
+</form>
+
+<TodoItem/>
